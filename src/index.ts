2c0603f9a0ce044ec7c2dd01e06710a40c15fe00
Mazen Amir <49759150+Selvster@users.noreply.github.com>
2025-04-29 18:43:45 +0300

    Logging

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import { bootstrap, runMigrations } from '@vendure/core';
 import { config } from './vendure-config';
 
 runMigrations(config)
-    .then(() => bootstrap(config))
-    .catch(err => {
-        console.log(err);
-    });
+  .then(() => bootstrap(config))
+  .then(app => {
+    console.log(`✅ Vendure is running on http://${config.apiOptions.hostname}:${config.apiOptions.port}`);
+  })
+  .catch(err => {
+    console.error('❌ Error starting Vendure:', err);
+  });
